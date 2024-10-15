30867ebbfb4f3a36c34a23654b2c3f1eb6251bb8
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-15 17:24:39 -0300

    Cadastro no SAP dos filmes, assim como as capas dos DVDs. Ajuste na tela inicial para exibir apenas 12 filmes

--- core/catalog/views.py
+++ core/catalog/views.py
@@ -1,70 +1,46 @@
 from django.shortcuts import render
-
-catalog = [
-    {
-        'Title':'The Dark Knight',
-        'Poster': '1.jpg',
-        'Available': False,
-        'Watched' : False
-    },
-    {
-        'Title': 'Inception',
-        'Poster': '2.jpg',
-        'Available': True,
-        'Watched': True
-    },
-    {
-        'Title': 'Fight Club',
-        'Poster': '3.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'Interstellar',
-        'Poster': '4.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'The Matrix',
-        'Poster': '5.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'The Lord of the Rings: The Fellowship of the Ring',
-        'Poster': '6.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'The Lord of the Rings: The Return of the King',
-        'Poster': '7.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'The Dark Knight Rises',
-        'Poster': '8.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'Se7en',
-        'Poster': '9.jpg',
-        'Available': True,
-        'Watched': False
-    },
-    {
-        'Title': 'The Lord of the Rings: The Two Towers',
-        'Poster': '10.jpg',
-        'Available': True,
-        'Watched': False
-    },
-
-]
+import requests 
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+import os
 
 def home(request):
+    
+    load_dotenv()
+    # Configurações do endpoint SAP
+    sap_url = "http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/CatalogSet?$top=12&$skip=0"
+    sap_ep = 'http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/'
+    usuario = os.getenv('sap_user')
+    senha = os.getenv('sap_pass')
+    
+    # Autenticação
+    auth = HTTPBasicAuth(usuario, senha) # type: ignore
+
+    # Pegar o token CSRF e cookies de sessão
+    session = requests.Session()  # Usar session para persistir cookies
+    token_response = session.get(
+        sap_ep, headers={"X-CSRF-Token": "Fetch"}, auth=auth)
+
+    csrf_token = token_response.headers.get("X-CSRF-Token")
+    catalog = []  # Caso de erro, passar um array vazio
+
+    # Certifique-se de que o token foi capturado
+    if csrf_token:
+        headers = {
+            "Content-Type": "application/json",  # Usar JSON
+            "Accept": "application/json",
+            "X-CSRF-Token": csrf_token
+        }
+        
+        # Fazer a requisição para o CatalogSet
+        response = session.get(sap_url, headers=headers, auth=auth)
+
+        # Checar se a resposta foi bem-sucedida
+        if response.status_code == 200:
+            # Parsear os dados JSON retornados
+            catalog = response.json().get('d', {}).get('results', [])
+        
+    
     return render(request, 'home.html', {'movies':catalog})
 
 def about(request):
