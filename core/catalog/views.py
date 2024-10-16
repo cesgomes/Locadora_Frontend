b6e7605697d10a35f395ebc6aacd88e2a59f3a32
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-10-15 23:40:30 -0300

    Exibição dos textos (descrição) das categorias

--- core/catalog/views.py
+++ core/catalog/views.py
@@ -4,90 +4,122 @@ from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 import os
 
+# Carregar as variáveis de ambiente
+load_dotenv()
 
-def movie_detail(request, id):
-    # Carregar as variáveis de ambiente
-    load_dotenv()
 
-    # Configurações do SAP
-    sap_ep = 'http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/'
+def get_categories(session,  auth):
+    
+
+    sap_ep = 'http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/GenresSet'
+    csrf_token = get_csrf_token(session, sap_ep, auth)
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-CSRF-Token": csrf_token
+    }
+    response = session.get(sap_ep, headers=headers, auth=auth)
+    if response.status_code == 200:
+        cats =  response.json().get('d', {})
+        cats = {item['Id']: item['Genre'] for item in cats['results']}  
+        return cats
+    return {}
+    
+def get_sap_auth():
+    """
+    Retorna a autenticação básica para o SAP usando variáveis de ambiente.
+    """
     usuario = os.getenv('sap_user')
     senha = os.getenv('sap_pass')
+    return HTTPBasicAuth(usuario, senha) # type: ignore
 
-    # Corrigir a URL (remover barra dupla)
-    sap_url = f"http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/CatalogSet({
-        id})"
+def get_csrf_token(session, sap_ep, auth):
+    """
+    Obtém o token CSRF necessário para as requisições ao SAP.
+    
+    :param session: Sessão de requisição HTTP.
+    :param sap_ep: Endpoint base do SAP.
+    :param auth: Autenticação HTTP.
+    :return: Token CSRF ou None se não for possível obtê-lo.
+    """
+    token_response = session.get(sap_ep, headers={"X-CSRF-Token": "Fetch"}, auth=auth)
+    return token_response.headers.get("X-CSRF-Token")
 
-    # Autenticação
-    auth = HTTPBasicAuth(usuario, senha)  # type: ignore
+def fetch_data_from_sap(session, url, headers, auth):
+    """
+    Faz uma requisição GET para o SAP e retorna os dados JSON.
+    
+    :param session: Sessão de requisição HTTP.
+    :param url: URL para a requisição.
+    :param headers: Cabeçalhos HTTP.
+    :param auth: Autenticação HTTP.
+    :return: Dados JSON da resposta ou um dicionário vazio em caso de erro.
+    """
+    response = session.get(url, headers=headers, auth=auth)
+    if response.status_code == 200:
+        return response.json().get('d', {})
+    return {}
 
-    # Iniciar uma sessão para persistir cookies
+def movie_detail(request, id):
+    """
+    View para exibir os detalhes de um filme específico.
+    
+    :param request: Objeto de requisição HTTP.
+    :param id: ID do filme.
+    :return: Renderização da página de detalhes do filme.
+    """
+    sap_ep = 'http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/'
+    sap_url = f"http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/CatalogSet({id})"
+    auth = get_sap_auth()
     session = requests.Session()
-
-    # Requisição para obter o token CSRF
-    token_response = session.get(
-        sap_ep, headers={"X-CSRF-Token": "Fetch"}, auth=auth
-    )
-
-    csrf_token = token_response.headers.get("X-CSRF-Token")
-
+    csrf_token = get_csrf_token(session, sap_ep, auth)
+    cats = get_categories(session=session, auth=auth)
     detail = {}
     if csrf_token:
         headers = {
-            "Content-Type": "application/json",  # Usar JSON
+            "Content-Type": "application/json",
             "Accept": "application/json",
             "X-CSRF-Token": csrf_token
         }
+        detail = fetch_data_from_sap(session, sap_url, headers, auth)
+    detail['Genre'] = [int(cat) for cat in detail['Genre'].split(',')]
+    detail['Genre'] = [cats[cat_id] for cat_id in detail['Genre']]
+    detail['Genre'] = ", ".join(detail['Genre'])
 
-        # Fazer a requisição para o CatalogSet com a chave
-        response = session.get(sap_url, headers=headers, auth=auth)
-
-        # Checar se a resposta foi bem-sucedida
-        if response.status_code == 200:
-            # Como é um único registro, acessar o objeto diretamente
-            detail = response.json().get('d', {})
-
-    return render(request, 'movie_detail.html', {'detail': detail})
-
+    return render(request, 'movie_detail.html', {'detail': detail, 'cats': cats})
 
 def home(request):
-
-    load_dotenv()
-    # Configurações do endpoint SAP
+    """
+    View para exibir a página inicial com uma lista de filmes.
+    
+    :param request: Objeto de requisição HTTP.
+    :return: Renderização da página inicial.
+    """
+    
+    auth = get_sap_auth()
+    session = requests.Session()
     sap_url = "http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/CatalogSet?$top=12&$skip=0"
     sap_ep = 'http://localhost:8000/sap/opu/odata/SAP/Z_BLOCKBUSTER_SRV/'
-    usuario = os.getenv('sap_user')
-    senha = os.getenv('sap_pass')
+    cats = get_categories(session=session, auth=auth)    
+    csrf_token = get_csrf_token(session, sap_ep, auth)
 
-    # Autenticação
-    auth = HTTPBasicAuth(usuario, senha)  # type: ignore
 
-    # Pegar o token CSRF e cookies de sessão
-    session = requests.Session()  # Usar session para persistir cookies
-    token_response = session.get(
-        sap_ep, headers={"X-CSRF-Token": "Fetch"}, auth=auth)
-
-    csrf_token = token_response.headers.get("X-CSRF-Token")
-    catalog = []  # Caso de erro, passar um array vazio
-
-    # Certifique-se de que o token foi capturado
+    catalog = []
     if csrf_token:
         headers = {
-            "Content-Type": "application/json",  # Usar JSON
+            "Content-Type": "application/json",
             "Accept": "application/json",
             "X-CSRF-Token": csrf_token
         }
+        catalog = fetch_data_from_sap(session, sap_url, headers, auth).get('results', [])
 
-        # Fazer a requisição para o CatalogSet
-        response = session.get(sap_url, headers=headers, auth=auth)
-
-        # Checar se a resposta foi bem-sucedida
-        if response.status_code == 200:
-            # Parsear os dados JSON retornados
-            catalog = response.json().get('d', {}).get('results', [])
-
-    return render(request, 'home.html', {'movies': catalog})
-
+    return render(request, 'home.html', {'movies': catalog, 'cats':cats})
 
 def about(request):
+    """
+    View para exibir a página 'Sobre'.
+    
+    :param request: Objeto de requisição HTTP.
+    :return: Renderização da página 'Sobre'.
+    """
     return render(request, 'about.html', {})
